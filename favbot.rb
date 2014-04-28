018d448a11f1b3bed0e6f560a988ce1b776d6ac7
Arsalan Malik <malarsalan@gmail.com>
2014-04-28 15:02:18 -0400

    Update favbot.rb

--- favbot.rb
+++ favbot.rb
@@ -1,18 +1,10 @@
 require 'Twitter' #gem install twitter
+require './config'
 while true
     begin
-        # Create a read write application from : 
-        # https://apps.twitter.com
-        # authenticate it for your account
-        # fill in the following
-        config = {
-            consumer_key:        'ZfruTPK51bB6VrhTZAJnpn8OH',
-            consumer_secret:     'bhS27kbr6lsnBgbTL6ry16V8YtElGoGHyySryxGnDGuE68cPDG',
-            access_token:        '2493510627-PKugXTm43e0oKiEHpg0UE35nbG2oG4hvGCvR2lj',
-            access_token_secret: 'T8qEweS296vLkz7bJByEh1ihN6sWxeulhDJO1x0WmL0u7'
-        }
-        rClient = Twitter::REST::Client.new config
-        sClient = Twitter::Streaming::Client.new(config)
+
+        rClient = Twitter::REST::Client.new $config
+        sClient = Twitter::Streaming::Client.new($config)
 
         # topics to watch
         topics = ['#bored', '#phonegap']
